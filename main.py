896199697ece0279b4590e50dc3711812c222e40
Simon <113543413+Mister-DS@users.noreply.github.com>
2025-01-02 11:55:46 +0100

    modification pour n'afficher que l'ip

--- main.py
+++ main.py
@@ -2,32 +2,29 @@
 import argparse
 import subprocess
 import platform
+import re
 
 
-def traceroute(ip_address, progressive=False, output_file=None, input_file=None):
-    # Si un fichier d'entrée est spécifié, on lit son contenu
-    if input_file:
-        try:
-            with open(input_file, 'r', encoding='utf-8') as file:
-                content = file.read()
-                print(f"Contenu du fichier '{input_file}':\n{content}")
-        except Exception as e:
-            print(f"Erreur lors de la lecture du fichier '{input_file}': {e}")
-            return
-
+def traceroute(ip_address, progressive=False, output_file=None):
     # Détecte le système d'exploitation et utilise la commande appropriée
     if platform.system().lower() == "windows":
-        cmd = ['tracert', ip_address]  # Commande tracert pour Windows
+        cmd = ['tracert', ip_address]  # pour Windows
     else:
-        cmd = ['traceroute', ip_address]  # Commande traceroute pour Linux/macOS
+        cmd = ['traceroute', ip_address]  # pour Linux/macOS
 
     try:
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
         results = []
+        compte = 0 #Initialise un compteur pour voir où on en est
         for line in process.stdout:
-            print(line.strip())  # Affiche chaque ligne de la sortie
-            results.append(line.strip())  # Ajoute la ligne aux résultats
+            # Extraction des adresses IP avec une expression régulière
+            match = re.search(r"\b(?:\d{1,3}.){3}\d{1,3}\b", line)
+            if match:
+                ip = match.group(0)
+                compte+=1
+                print(f'{compte} : {ip}')  # Affiche uniquement l'adresse IP
+                results.append(ip)
 
             if progressive:
                 input("Appuyez sur Entrée pour continuer...")
@@ -48,12 +45,11 @@ def main():
     parser.add_argument("ip_address", type=str, help="L'adresse IP ou le nom de domaine à tracer.")
     parser.add_argument("-p", "--progressive", action="store_true", help="Activer l'exécution progressive.")
     parser.add_argument("-o", "--output-file", type=str, help="Fichier dans lequel enregistrer les résultats.")
-    parser.add_argument("-i", "--input-file", type=str, help="Fichier à lire avant d'exécuter traceroute.")
 
     args = parser.parse_args()
 
     # Exécution de la fonction traceroute avec les options fournies
-    traceroute(args.ip_address, args.progressive, args.output_file, args.input_file)
+    traceroute(args.ip_address, args.progressive, args.output_file)
 
 
 if __name__ == "__main__":
